ba492845a4f470365d244aa63beb7aaefd73c085
Nova06244 <nova06244@gmail.com>
2025-08-04 22:22:38 +0200

    Update app2.py

--- app2.py
+++ app2.py
@@ -44,7 +44,7 @@ with st.form("add_trade_form"):
         date = st.date_input("Date", value=datetime.now()).strftime("%d/%m/%Y")
         session = st.selectbox("Session", ["OPR 9h", "OPR 15h30", "OPRR 18h30"])
     with col2:
-        actif = st.text_input("Actif", value="EUR/USD")
+        actif = st.text_input("Actif", value="XAU/USD")
         resultat = st.selectbox("Résultat", ["TP", "SL"])
     with col3:
         risk = st.number_input("Risk (%)", min_value=0.0, step=0.01, format="%.2f")
